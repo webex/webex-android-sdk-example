c1473447e31b7b7665e2adbe953e167044c4626e
Allen Shaw <xionxiao@cisco.com>
2017-07-27 16:11:19 +0800

    [SSDK-1041] Add Spark id login

--- app/src/main/java/com/cisco/sparksdk/sparkkitchensink/EnterActivity.java
+++ app/src/main/java/com/cisco/sparksdk/sparkkitchensink/EnterActivity.java
@@ -1,13 +1,18 @@
 package com.cisco.sparksdk.sparkkitchensink;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.EventLogTags;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.cisco.spark.android.authenticator.OAuth2AccessToken;
+import com.ciscospark.auth.AuthorizeListener;
+import com.ciscospark.auth.OAuth2Authenticator;
+import com.ciscospark.common.SparkError;
 
 public class EnterActivity extends AppCompatActivity {
 
@@ -25,6 +30,34 @@ public class EnterActivity extends AppCompatActivity {
 
         Log.i(TAG, "onCreate: ->start");
 
+        buttonSpark = (Button) findViewById(R.id.buttonSpark);
+        buttonSpark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "spark id authorize");
+                String clientId = "Cc580d5219555f0df8b03d99f3e020381eae4eee0bad1501ad187480db311cce4";
+                String clientSec = "d4e9385b2e5828eef376077995080ea4aa42b5c92f1b6af8f3a59fc6a4e79f6a";
+                String redirect = "AndroidDemoApp://response";
+                String scope = "spark:all spark:kms";
+                WebView webView = (WebView) findViewById(R.id.OAuthWebView);
+                OAuth2Authenticator authenticator = new OAuth2Authenticator(clientId,clientSec,redirect,scope);
+                ((KitchenSinkApplication)getApplication()).mSpark.setStrategy(authenticator);
+                if (!authenticator.isAuthorized()) {
+                    authenticator.authorize(webView, new AuthorizeListener() {
+                        @Override
+                        public void onSuccess(OAuth2AccessToken oAuth2AccessToken) {
+                            startActivity(new Intent(EnterActivity.this, RegistryActivity.class));
+                            finish();
+                        }
+
+                        @Override
+                        public void onFailed(SparkError<AuthError> sparkError) {
+                            Toast.makeText(EnterActivity.this, "Authorize error: " + sparkError.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
 
         buttonJWT = (Button) findViewById(R.id.buttonJWT);
         buttonJWT.setOnClickListener(new View.OnClickListener() {
@@ -37,9 +70,9 @@ public class EnterActivity extends AppCompatActivity {
 
                 EnterActivity.this.startActivity(intent);
 
-                KitchenSinkApplication myapplication = (KitchenSinkApplication)getApplication();
+                KitchenSinkApplication myapplication = (KitchenSinkApplication) getApplication();
 
-                if(myapplication.mSpark != null){
+                if (myapplication.mSpark != null) {
 
                     Log.i(TAG, "mSpark is created ");
 
@@ -50,7 +83,7 @@ public class EnterActivity extends AppCompatActivity {
 
                     //Toast.makeText(EnterActivity.this, "mSpark is created", Toast.LENGTH_SHORT).show();
 
-                }else{
+                } else {
 
                     Log.i(TAG, "mSpark is null ");
 
