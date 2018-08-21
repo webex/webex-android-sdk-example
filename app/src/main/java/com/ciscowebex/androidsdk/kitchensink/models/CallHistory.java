cd4144f50b76453802e6619b079ee3908d8e33c1
QimingDeng <qimdeng@cisco.com>
2018-08-21 11:30:30 +0800

    [SPARK-28284][Android Webex SDK]MultiStream feature for KitchenSink app

--- app/src/main/java/com/ciscowebex/androidsdk/kitchensink/models/CallHistory.java
+++ app/src/main/java/com/ciscowebex/androidsdk/kitchensink/models/CallHistory.java
@@ -42,7 +42,7 @@ public class CallHistory {
 
     private String direction;
 
-    @Generated(hash = 939102572)
+    @Generated()
     public CallHistory(Long id, String email, String person, Date date,
             String direction) {
         this.id = id;
@@ -52,7 +52,7 @@ public class CallHistory {
         this.direction = direction;
     }
 
-    @Generated(hash = 390849564)
+    @Generated()
     public CallHistory() {
     }
 
